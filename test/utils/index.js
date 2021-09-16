7624d9a45740986130fa2495783119cc9e2edaa5
Philippe Dumoet <philippe@dumo.net>
2021-09-16 21:16:50 +0200

    Create time utils

--- test/utils/index.js
+++ test/utils/index.js
@@ -1,3 +1,30 @@
+const { network, ethers } = require('hardhat')
+
+const seconds = (x) => x
+const minutes = (x) => 60 * seconds(x)
+const hours = (x) => 60 * minutes(x)
+const days = (x) => 24 * hours(x)
+const weeks = (x) => 7 * days(x)
+
+const duration = { seconds, minutes, hours, days, weeks }
+
+const time = {
+  duration,
+  latest: async () => {
+    const blockNumber = await ethers.provider.getBlockNumber()
+    const { timestamp } = await ethers.provider.getBlock(blockNumber)
+    return timestamp
+  },
+  increaseBy: async (seconds) => {
+    await network.provider.send('evm_increaseTime', [seconds])
+    await network.provider.send('evm_mine')
+  },
+  increaseTo: async (timestamp) => {
+    await network.provider.send('evm_setNextBlockTimestamp', [timestamp])
+    await network.provider.send('evm_mine')
+  }
+}
+
 const addresses = (val) => {
   if (val instanceof Array) return val.map((account) => account.address)
   return val.address
@@ -5,4 +32,4 @@ const addresses = (val) => {
 
 const SAFE_TRANSFER_FROM = 'safeTransferFrom(address,address,uint256)'
 
-module.exports = { addresses, SAFE_TRANSFER_FROM }
+module.exports = { time, addresses, SAFE_TRANSFER_FROM }
